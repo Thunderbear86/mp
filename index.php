ddaf57e42dd6265f7312161d5c66b54cb3c7a341
Thunderbear86 <116293845+Thunderbear86@users.noreply.github.com>
2023-04-23 22:19:34 +0200

    Restart

--- index.php
+++ index.php
@@ -14,19 +14,104 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
-<body>
+<body class="bg-light">
 
-<!--header-->
+<header>
 
-<div class="container-fluid">
-    <div class="row">
-        <div class="col">
-            <?php include 'menu.php'; ?>
-        </div>
-    </div>
-</div>
+    <ul class="nav justify-content-end">
+        <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Active</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        </li>
+    </ul>
+
+</header>
+
 
 <!-- Indhold -->
+
+<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="img/carousel/identitet.jpeg" class="d-block w-100" alt="identitet">
+            <div class="carousel-caption d-none d-md-block">
+                <h5>Identitet</h5>
+                <p>Some representative placeholder content for the first slide.</p>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img src="img/carousel/positionering.jpeg" class="d-block w-100" alt="positionering">
+            <div class="carousel-caption d-none d-md-block">
+                <h5>Positionering</h5>
+                <p>Some representative placeholder content for the second slide.</p>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img src="img/carousel/team.jpeg" class="d-block w-100" alt="team">
+            <div class="carousel-caption d-none d-md-block">
+                <h5>Team</h5>
+                <p>Some representative placeholder content for the third slide.</p>
+            </div>
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+</div>
+
+
+
+
+<div class="container">
+    <div class="row">
+        <div>
+            <h3>Identitet</h3>
+                <div class="card shadow col-sm-12 col-md-8 col-lg-4">
+                    <img class="card-img-top" src="img/baggrund%20v2.png" alt="test" />
+
+                    <div class="card-body">
+                        Tekst
+                    </div>
+                </div>
+        </div>
+        <h3>Positionering</h3>
+            <div class="card shadow col-6">
+                <div class="card-body">
+                    Tekst
+                </div>
+            </div>
+        <h3>Kultur</h3>
+            <div class="card shadow">
+                <div class="card-body">
+                    Tekst
+                </div>
+            </div>
+        <h3>Budskab & Tone</h3>
+            <div class="card shadow">
+                <div class="card-body">
+                    Tekst
+                </div>
+            </div>
+    </div>
+</div>
 
 <div class="container">
     <div class="row">
@@ -34,13 +119,13 @@
             <h3>Identitet</h3>
         </div>
         <div class="col">
-            <h3>Branding strategi</h3>
+            <h3>Positionering</h3>
         </div>
         <div class="col">
-            <h3>Ledelses Coaching</h3>
+            <h3>Kultur</h3>
         </div>
         <div class="col">
-            <h3>Positiv forstærkning</h3>
+            <h3>Budskab & Tone</h3>
         </div>
     </div>
 </div>
@@ -51,7 +136,7 @@
             <h3>AI din virksomhed</h3>
         </div>
         <div class="col">
-            <h3>Branding strategi</h3>
+            <h3>Segmentering</h3>
         </div>
         <div class="col">
             <h3>Ledelses Coaching</h3>
