f8f0de00775063870fe2f915e8537e20f025e2c1
Thunderbear86 <116293845+Thunderbear86@users.noreply.github.com>
2023-06-17 12:28:36 +0200

    Init 2?

--- footer.php
+++ footer.php
@@ -1,8 +1,12 @@
 <?php echo
-'<footer id="main-footer">
+'<footer class="mt-3" id="main-footer">
     <div class="container">
         <div class="row">
-            <h6 class="text-center">Fenris &copy; 2023</h6> 
+            <div class="col">
+                <h6 class="text-start col-4">Metaperspektiv &copy; 2023</h6> 
+                <h6 class="text-center col-4">Info@metaperspektiv.dk</h6> 
+                <h6 class="text-end col-4">CVR nr.:</h6>
+            </div>
         </div>
     </div>
   </footer>';
--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <title>Metaperspektiv</title>
 
     <meta name="robots" content="All">
-    <meta name="author" content="Udgiver">
+    <meta name="author" content="Thorbjørn Wagner">
     <meta name="copyright" content="Information om copyright">
 
     <link href="css/styles.css" rel="stylesheet" type="text/css">
@@ -17,81 +17,138 @@
 <body class="bg-light">
 
 <header>
+<div class="container">
+    <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+                <ul class="nav justify-content-end">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Top</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Identitet</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Positionering</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Team</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Blog</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Om Os</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Kontakt</a>
+                    </li>
 
-    <ul class="nav justify-content-end">
-        <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Active</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-        </li>
-    </ul>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</div>
+
 
 </header>
 
 
 <!-- Indhold -->
 
-<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
-    <div class="carousel-indicators">
-        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+
+
+<div class="container mt-3">
+    <div id="carouselExampleCaptions" class="carousel slide mb-3" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="img/carousel/identitet.jpeg" class="d-block w-100" alt="identitet">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Identitet</h5>
+                    <p>Some representative placeholder content for the first slide.</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="img/carousel/positionering.jpeg" class="d-block w-100" alt="positionering">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Positionering</h5>
+                    <p>Some representative placeholder content for the second slide.</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="img/carousel/team.jpeg" class="d-block w-100" alt="team">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Team</h5>
+                    <p>Some representative placeholder content for the third slide.</p>
+                </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
     </div>
-    <div class="carousel-inner">
-        <div class="carousel-item active">
-            <img src="img/carousel/identitet.jpeg" class="d-block w-100" alt="identitet">
-            <div class="carousel-caption d-none d-md-block">
-                <h5>Identitet</h5>
-                <p>Some representative placeholder content for the first slide.</p>
-            </div>
-        </div>
-        <div class="carousel-item">
-            <img src="img/carousel/positionering.jpeg" class="d-block w-100" alt="positionering">
-            <div class="carousel-caption d-none d-md-block">
-                <h5>Positionering</h5>
-                <p>Some representative placeholder content for the second slide.</p>
-            </div>
-        </div>
-        <div class="carousel-item">
-            <img src="img/carousel/team.jpeg" class="d-block w-100" alt="team">
-            <div class="carousel-caption d-none d-md-block">
-                <h5>Team</h5>
-                <p>Some representative placeholder content for the third slide.</p>
-            </div>
-        </div>
-    </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-    </button>
 </div>
-
-
 
 
 <div class="container">
     <div class="row">
-        <div>
-            <h3>Identitet</h3>
-                <div class="card shadow col-sm-12 col-md-8 col-lg-4">
-                    <img class="card-img-top" src="img/baggrund%20v2.png" alt="test" />
-
-                    <div class="card-body">
-                        Tekst
-                    </div>
-                </div>
+        <div class="col" style="background-image: url('img/heroimg.jpeg'); background-size: cover; background-position: center center;">
+            <div class="text-center text-white">
+                <h1>Din virksomhed - Din mulighed!</h1>
+                <p>Fremtiden er bæredygtig! Hvad er din superkraft?</p>
+                <p>Du er måske allerede klar over det. Din kommende købekraft sætter fokus på brands med samvittighed!</p>
+                <p>Socialt ansvar i lokalområdet - Grøn produktion - Nul-udledning - Investering i fremtiden</p>
+            </div>
         </div>
+    </div>
+</div>
+
+
+<div class="container mt-3">
+    <div class="row gy-2">
+
+        <div class="col-sm-12 col-md-6 col-lg-6">
+            <h3 class="fw-bold">Identitet</h3>
+            <p>Test tekst</p>
+            <button>Se indlæg</button>
+        </div>
+        <div class="col-sm-12 col-md-6 col-lg-6">
+            <img class="img-fluid" src="img/carousel/identitet.jpeg" alt="identitet" />
+        </div>
+
+
+
+        <div class="col-sm-12 col-md-6 col-lg-6">
+            <img class="img-fluid" src="img/carousel/positionering.jpeg" alt="positionering" />
+        </div>
+        <div class="col-sm-12 col-md-6 col-lg-6">
+            <h3 class="fw-bold">Positionering</h3>
+            <p>Test tekst</p>
+            <button>Se indlæg</button>
+        </div>
+
+        <div class="col-sm-12 col-md-6 col-lg-6">
+            <h3 class="fw-bold">Kultur</h3>
+            <p>Test tekst</p>
+            <button>Se indlæg</button>
+        </div>
+        <div class="col-sm-12 col-md-6 col-lg-6">
+            <img class="img-fluid" src="img/carousel/team.jpeg" alt="kultur" />
+        </div>
+
         <h3>Positionering</h3>
             <div class="card shadow col-6">
                 <div class="card-body">
@@ -155,6 +212,9 @@
 
 
 <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+
+<script type="module">import * as THREE from 'three';</script>
+
 </body>
 </html>
 
